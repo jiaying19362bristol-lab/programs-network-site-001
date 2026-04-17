801b1917688aa87be9a1dbeb4d095e0f8981c606
jiaying19362bristol-lab <jiaying19362bristol-lab@users.noreply.github.com>
2026-04-17 18:51:25 +0800

    Fix cloud site registry format

--- cloud_sites.js
+++ cloud_sites.js
@@ -1,5 +1,7 @@
-﻿window.__CLOUD_SITES__ = {
-    "repo":  "programs-network-site-004",
-    "url":  "https://jiaying19362bristol-lab.github.io/programs-network-site-004/",
-    "lastUpdatedAt":  "2026-04-17T18:10:02.1321092+08:00"
-};
+window.__CLOUD_SITES__ = [
+  {
+    "repo": "programs-network-site-001",
+    "url": "https://jiaying19362bristol-lab.github.io/programs-network-site-001/",
+    "lastUpdatedAt": "2026-04-17T18:25:02.5324901+08:00"
+  }
+];
