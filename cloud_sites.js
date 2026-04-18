86149e9c1e737e63e06645bf063c005e4b4b942f
jiaying19362bristol-lab <jiaying.19362bristol@gmail.com>
2026-04-18 11:26:56 +0800

    Update cloud registry 2026-04-18T03:26:52.466Z

--- cloud_sites.js
+++ cloud_sites.js
@@ -1,5 +1,7 @@
-﻿window.__CLOUD_SITES__ = {
-    "repo":  "programs-network-site-001",
-    "url":  "https://jiaying19362bristol-lab.github.io/programs-network-site-001/",
-    "lastUpdatedAt":  "2026-04-17T18:50:02.1454522+08:00"
-};
+window.__CLOUD_SITES__ = [
+  {
+    "repo": "programs-network-site-001",
+    "url": "https://jiaying19362bristol-lab.github.io/programs-network-site-001/",
+    "lastUpdatedAt": "2026-04-18T03:26:52.466Z"
+  }
+];
