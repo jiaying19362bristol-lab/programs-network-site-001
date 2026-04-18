6dafa494fbbce64a063b6212783732a267e67429
jiaying19362bristol-lab <jiaying.19362bristol@gmail.com>
2026-04-18 12:02:14 +0800

    Sync cloud_sites.js from local browser state

--- cloud_sites.js
+++ cloud_sites.js
@@ -1,5 +1,5 @@
-﻿window.__CLOUD_SITES__ = {
-    "repo":  "programs-network-site-001",
-    "url":  "https://jiaying19362bristol-lab.github.io/programs-network-site-001/",
-    "lastUpdatedAt":  "2026-04-17T19:20:02.1456238+08:00"
+window.__CLOUD_SITES__ = {
+  "repo": "programs-network-site-001",
+  "url": "https://jiaying19362bristol-lab.github.io/programs-network-site-001/",
+  "lastUpdatedAt": "2026-04-18T12:02:00+08:00"
 };
